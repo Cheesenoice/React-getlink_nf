be5c32ff67fe4985053dfa28a328e2f8fec5bd2d
HUYNH HUU TRI <huynhhuutri2004@gmail.com>
2024-09-24 21:55:40 +0700

    css change

--- client/src/Navbar.jsx
+++ client/src/Navbar.jsx
@@ -8,13 +8,32 @@ const Navbar = () => {
         <a href="/" className="navbar-logo">
           Netflix Tool
         </a>
-        <a
-          href="https://facebook.com/huutrii"
-          className="facebook-button"
-          target="_blank"
-        >
-          Facebook
-        </a>
+        <div className="social-buttons">
+          <a
+            href="https://t.me/trisvjp"
+            className="telegram-button"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Telegram_logo.svg/768px-Telegram_logo.svg.png?20220101141644"
+              alt="Telegram"
+              className="telegram-icon"
+            />
+          </a>
+          <a
+            href="https://facebook.com/huutrii"
+            className="facebook-button"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/2023_Facebook_icon.svg/900px-2023_Facebook_icon.svg.png?20231011122028"
+              alt="Facebook"
+              className="facebook-icon"
+            />
+          </a>
+        </div>
       </div>
     </nav>
   );
